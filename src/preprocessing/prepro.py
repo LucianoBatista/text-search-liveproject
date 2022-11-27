87339b92b0c13159e61375e6bbede8300f6f91f0
Luba <luciano.eq@outlook.com>
2022-11-27 09:34:26 -0300

    :sparkles: milestone-1.

---
+++ src/preprocessing/prepro.py
@@ -0,0 +1,38 @@
+import spacy
+
+
+class TextPrep:
+    def __init__(self, text: str) -> None:
+        self.text = text
+        self.nlp = spacy.load("en_core_web_sm")
+        self.doc = None
+        self.final_tokens = []
+
+    def get_doc(self):
+        doc = self.nlp(self.text)
+        return doc
+
+    def lowercasing(self):
+        self.text = self.text.lower()
+
+    def get_lemmas(self):
+        self.doc = self.get_doc()
+        for token in self.doc:
+            if not token.is_punct and not token.is_stop:
+                self.final_tokens.append(token.lemma_)
+
+
+def lowercasing():
+    ...
+
+
+def remove_stop_words():
+    ...
+
+
+def remove_punckt():
+    ...
+
+
+def remove_unclassified_lemmas():
+    ...
